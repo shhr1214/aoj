9777e3158bb91fa8f90d0da08f577f1705ced1ee
Sohei Hara <souhei0079@gmail.com>
2016-04-02 23:51:31 +0900

    add insertion_sort.rb

--- ruby/insertion_sort.rb
+++ ruby/insertion_sort.rb
@@ -1,5 +1,19 @@
-N = gets.to_i
-A = gets.split(" ")
+def insertion_sort(n, a)
+  for i in 0..n-1
+    v = a[i]
+    j = i - 1
+    while j >= 0 && a[j] > v
+      a[j+1] = a[j]
+      j = j - 1
+    end
+    a[j+1] = v
+    print a.join(" ") + "\n"
+  end
+end
 
-p N
-p A
+if __FILE__ == $0
+  n = gets.to_i
+  a = gets.split(" ").map(&:to_i)
+  p a
+  insertion_sort(n, a)
+end
